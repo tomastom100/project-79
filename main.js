6d8135d9beb2d803fa1a801fd69fa7c60803a7d5
tomastom100 <86069280+tomastom100@users.noreply.github.com>
2021-09-08 11:28:01 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+menu_list_array = ["Veg Margherita Pizza","Ham and cheese","Hawaiian","Ham chicken","Mexican","Spicy pizza","Paisa pizza","Three cheeses","Vegetarian","Neapolitan","Carbonara","Funghi"//add more items
+                    ];
+
+function getmenu(){
+var htmldata;
+menu_list_array.sort();
+        for(var i=0;i<menu_list_array.length;i++){
+            htmldata=htmldata+ menu_list_array[i] + '<br>'
+        }
+        document.getElementById("display_menu").innerHTML = htmldata;
+        //give the appropriate id name as display_menu  
+}
+
+function add_item(){
+var htmldata;
+var item=document.getElementById("add_item").value;
+menu_list_array.sort();
+        // use the sort function as - menu_list_array.sort();
+        htmldata=""
+        for(var i=0;i<menu_list_array.length;i++){
+            htmldata=htmldata+imgtags+ menu_list_array[i]+'<br>';
+        }
+         document.getElementById("display_addedmenu").innerHTML = htmldata;		
+      }
+
+function add_top(){
+    var item=document.getElementById("add_item").value;
+    menu_list_array.push(item);
+	add_item();
+}
